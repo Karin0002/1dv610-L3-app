39689ebb97b53e855b38f5d56a053deaa6c1e0f7
Karin Silfversparre <ks224ac@student.lnu.se>
2024-12-17 09:45:35 +0100

    Install dependencies and add structure

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export * from './classes/AnalogousThemeFactory.js'
+export * from './classes/ColorStyles.js'
+export * from './classes/ComplementaryThemeFactory.js'
+export * from './classes/MonochromeThemeFactory.js'
+export * from './classes/SplitComplementaryThemeFactory.js'
+export * from './classes/TriadicThemeFactory.js'
