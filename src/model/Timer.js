aeb758374e04979656c59cae195053c9530e2661
Karin Silfversparre <ks224ac@student.lnu.se>
2024-12-18 07:41:58 +0100

    Add Timer

---
+++ src/model/Timer.js
@@ -0,0 +1,38 @@
+export class Timer {
+  /**
+   * @type {Date}
+   */
+  #date
+
+  /**
+   * @type {Date}
+   */
+  #startTime
+  /**
+   * @type {Date}
+   */
+  #endTime
+
+  constructor (dateObject) {
+    this.#date = dateObject
+  }
+
+  startTimer () {
+    this.#resetFields()
+
+    this.#startTime = this.#date.now()
+  }
+
+  #resetFields () {
+    this.#startTime = ''
+    this.#endTime = ''
+  }
+
+  stopTimer () {
+    this.#endTime = this.#date.now()
+  }
+
+  getTime () {
+    return this.#endTime - this.#startTime 
+  }
+}
